66661c5cada1ef2a7d52aaf9267c057f18579041
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2022-08-08 16:24:42 +0300

    Raised the step count and removed most instances of the test method - it should get moved to the tests

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -19,15 +19,15 @@ void constexpr_and_runtime_test();
 
 int main()
 {
-    constexpr_and_runtime_test<64>();
-    constexpr_and_runtime_test<65>();
-    constexpr_and_runtime_test<66>();
+    //constexpr_and_runtime_test<64>();
+    //constexpr_and_runtime_test<65>();
+    //constexpr_and_runtime_test<66>();
     constexpr_and_runtime_test<67>();
 
-    constexpr_and_runtime_test<128>();
-    constexpr_and_runtime_test<129>();
-    constexpr_and_runtime_test<130>();
-    constexpr_and_runtime_test<131>();
+    //constexpr_and_runtime_test<128>();
+    //constexpr_and_runtime_test<129>();
+    //constexpr_and_runtime_test<130>();
+    //constexpr_and_runtime_test<131>();
 
     return 0;
 }
